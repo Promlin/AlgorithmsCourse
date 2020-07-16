5610e6efe4dad857ecd8fd3cbe1ed3049e64e63b
Irina <irinatyumyantseva@gmail.com>
2020-07-17 00:21:21 +0300

    заготовка

---
+++ AlgorithmsHomework/HW_4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace AlgorithmsHomework
+{
+    class HW_4
+    {
+        public static void RunExercises()
+        {
+            Console.WriteLine("Введите номер упражнения 1 - 8");
+            int exerciseNumber = int.Parse(Console.ReadLine());
+
+            switch (exerciseNumber)
+            {
+                case 1:
+                    //firstExercise();
+                    break;
+                case 2:
+                    //secondExercise();
+                    break;
+                default:
+                    Console.WriteLine("Ошибка при вводе. Введите число от 1 до 8");
+                    break;
+            }
+        }
+    }
+}
