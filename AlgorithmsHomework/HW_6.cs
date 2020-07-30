d50e969f50d09c61187786c75c6831e11063f350
Promlin <somebody2605@yandex.ru>
2020-07-30 12:58:35 +0300

    Упр 1

--- AlgorithmsHomework/HW_6.cs
+++ AlgorithmsHomework/HW_6.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace AlgorithmsHomework
 {
@@ -10,18 +8,57 @@ namespace AlgorithmsHomework
     {
         public static void RunExercises()
         {
-            Console.WriteLine("Введите номер упражнения 1 - 8");
+            Console.WriteLine("Введите номер упражнения 1 - 2");
             int exerciseNumber = int.Parse(Console.ReadLine());
 
             switch (exerciseNumber)
             {
                 case 1:
-                    //firstExercise();
+                    firstExercise();
+                    break;
+                case 2:
+                    secondExercise();
                     break;
                 default:
-                    Console.WriteLine("Ошибка при вводе. Введите число от 1 до 8");
+                    Console.WriteLine("Ошибка при вводе. Введите число от 1 до 2");
                     break;
             }
+        }
+
+        //Реализовать простейшую хеш-функцию. На вход функции подается строка, на выходе сумма кодов символов.
+        public static void firstExercise()
+        {
+            string sSourceData;
+            byte[] tmpSource;
+            byte[] tmpHash;
+
+            Console.WriteLine("Данные считываются из файла. Получившийся хэш-код: ");
+            sSourceData = "MySourceData";
+            tmpSource = ASCIIEncoding.ASCII.GetBytes(sSourceData);
+
+            tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+
+            Console.WriteLine(ByteArrayToString(tmpHash));
+            
+
+        }
+        static string ByteArrayToString(byte[] arrInput)
+        {
+            int i;
+            StringBuilder sOutput = new StringBuilder(arrInput.Length);
+            for (i = 0; i < arrInput.Length; i++)
+            {
+                sOutput.Append(arrInput[i].ToString("X2"));
+            }
+            return sOutput.ToString();
+        }
+
+        //Переписать программу, реализующую двоичное дерево поиска.
+        //а) Добавить в него обход дерева различными способами;
+        //б) Реализовать поиск в двоичном дереве поиска;
+        public static void secondExercise()
+        {
+
         }
     }
 }
