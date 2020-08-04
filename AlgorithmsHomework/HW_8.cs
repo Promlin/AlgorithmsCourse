2b4c4035a6a77b7af04d5afb375cb738172316c1
Promlin <somebody2605@yandex.ru>
2020-08-04 13:31:26 +0300

    заготовка

---
+++ AlgorithmsHomework/HW_8.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace AlgorithmsHomework
+{
+    class HW_8
+    {
+        public static void RunExercises()
+        {
+            Console.WriteLine("Введите номер упражнения 1 - 2");
+            int exerciseNumber = int.Parse(Console.ReadLine());
+
+            switch (exerciseNumber)
+            {
+                case 1:
+                    firstExercise();
+                    break;
+                case 2:
+                    secondExercise();
+                    break;
+                default:
+                    Console.WriteLine("Ошибка при вводе. Введите число от 1 до 2");
+                    break;
+            }
+        }
+
+        //Реализовать сортировку подсчетом
+        public static void firstExercise()
+        {
+
+        }
+
+        //Реализовать быструю сортировку.
+        public static void secondExercise()
+        {
+
+        }
+    }
+}
